049af20043b0402c0e694cab99f1893d728dab47
Iamneo <techadmin@iamneo.ai>
2023-10-01 18:25:51 +0000

    Backup Commit

--- springapp/src/controller/MedicineController.java
+++ springapp/src/controller/MedicineController.java
@@ -5,5 +5,19 @@ import java.util.List;
 import java.util.Map;
 public class MedicineController {
     private Map<Integer, Medicine> medicineMap=new HashMap<>();
+    private int currentId=1;
+    @PostMapping
+    public boolean addMedicine(@RequestBody Medicine medicine){
+        medicine.setMedicineId(currentId++);
+        medicineMap.put(medicine.getMedicineId(),medicine);
+        return true;
+    }
+    @PutMapping
+    public Medicine updateMedicine(@PathVariable int medicineId,@RequestBody Medicine updateMedicine){
+        if(medicineMap.containsKey(medicineId)){
+            Medicine existingMedicine=medicineMap.get(medicineId);
+            existingMedicine.setMedicineName(updated)
+        }
+    }
     
 }
